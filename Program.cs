c8c79d5af87d665bb02ec86a5d4b5c75e5898f83
IamPedrin <pedrin.luiz22@gmail.com>
2026-02-20 12:19:34 -0300

    [FEAT] Rotas de encurtamento de forma bruta e redirecionamento funcionando

--- Program.cs
+++ Program.cs
@@ -10,16 +10,30 @@ var app = builder.Build();
 
 app.MapGet("/", () => "CShortener!");
 
-app.MapPost("/api/shortener", (CreateUrlRequest request) =>
+app.MapPost("/api/shortener", (CreateUrlRequest request, AppDbContext db) =>
 {
     var urlObj = new Url(1234, request.OriginalUrl, "abc123");
+
+    db.Add(urlObj);
+    db.SaveChanges();
+
     return Results.Ok(new { url = $"site.com/{urlObj.ShortCode}" });
 });
 
-app.MapGet("/{shortCode}", (string shortCode) =>
-{
-    //urlObj.NewAcess();
-    return Results.Redirect("https://google.com", permanent: false);
+app.MapGet("/{shortCode}", (string shortCode, AppDbContext db) =>
+{   
+    var urlDb = db.Urls.FirstOrDefault(u => u.ShortCode == shortCode);
+
+    if(urlDb == null)
+    {
+        return Results.NotFound(new {erro = "Link não encontrado!"});
+    }
+    else
+    {
+        urlDb.NewAccess();
+        db.SaveChanges();
+        return Results.Redirect(urlDb.OriginalUrl);
+    }
 });
 
 
